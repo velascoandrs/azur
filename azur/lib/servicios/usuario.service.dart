de0f9b3b6b99507f89932e69604d878a8f14226a
Andrés Velasco <velasco.andrs@gmail.com>
2019-05-19 00:24:43 -0500

    formulario-login

---
+++ azur/lib/servicios/usuario.service.dart
@@ -0,0 +1,11 @@
+class UsuarioService {
+
+  save() async {
+    print('saving user using a web service');
+  }
+
+  // Servicio de login, retorna el Token y el Token de refrescamiento
+  login() async {
+    print('saving user using a web service');
+  }
+}
